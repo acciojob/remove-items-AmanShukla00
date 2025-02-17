405c94981f18700b84a96454b30abc2c98274fe1
AmanShukla00 <aman17101998@gmail.com>
2025-02-17 23:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("input[type='button']").addEventListener("click", function () {
+        let select = document.getElementById("colorSelect");
+        
+        if (select.selectedIndex !== -1) {
+            select.remove(select.selectedIndex);
+        }
+    });
+});
